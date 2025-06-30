4e846f646659fbb7b189dd7aee184ff4f7949ea0
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-06-30 12:31:35 -0300

    New projcts files

---
+++ src/main/java/com/kiev/algacoments/AlgacomentsApplication.java
@@ -0,0 +1,13 @@
+package com.kiev.algacoments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgacomentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgacomentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiev/algacoments/AlgacomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiev.algacoments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgacomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
